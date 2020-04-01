077c1f9784f6ab86e2623ca3e97f6a96136c1f28
Oranore <60654293+Oranore@users.noreply.github.com>
2020-04-01 11:52:01 +0200

    Quest 2 React done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        name="Oranore"
+        imgAvatar="https://randomuser.me/api/portraits/men/13.jpg"
+        isOnline
+      />
+      <Contact
+        name="John"
+        imgAvatar="https://randomuser.me/api/portraits/men/81.jpg"
+        isOnline={false}
+      />
+      <Contact
+        name="Florence"
+        imgAvatar="https://randomuser.me/api/portraits/women/76.jpg"
+        isOnline
+      />
     </div>
   );
 }
