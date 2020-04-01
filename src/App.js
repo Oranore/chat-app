5b5fa36dbc570d41d4f9339b013d62cc2b68d149
Oranore <60654293+Oranore@users.noreply.github.com>
2020-04-01 17:09:24 +0200

    TOP QUEST

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
-import './App.css';
-
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Oranore"
-        imgAvatar="https://randomuser.me/api/portraits/men/13.jpg"
-        isOnline
-      />
-      <Contact
-        name="John"
-        imgAvatar="https://randomuser.me/api/portraits/men/81.jpg"
-        isOnline={false}
-      />
-      <Contact
-        name="Florence"
-        imgAvatar="https://randomuser.me/api/portraits/women/76.jpg"
-        isOnline
-      />
+      <ContactList />
     </div>
   );
 }
